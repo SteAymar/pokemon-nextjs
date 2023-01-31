6e88810e20c9d9344cb200100dc99e463b0fb60d
Stefano Aymar <ste.aymar@gmail.com>
2023-01-31 11:17:51 -0300

    added canvas-confeti library

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
 import { GetStaticProps, NextPage, GetStaticPaths } from "next";
+
+import confetti from 'canvas-confetti';
+
 import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts";
 import { Pokemon } from "../../interfaces";
@@ -19,6 +22,20 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
   const onToggleFavorite = () => {
     localFavorites.toggleFavorite(pokemon.id);
     setIsInFavorites( !isInFavorites )
+
+    if ( isInFavorites ) return;
+
+    confetti ({
+      zIndex: 999,
+      particleCount: 100,
+      spread: 160,
+      angle: -100,
+      origin:{
+        x: 1,
+        y: 0,
+      }
+    })
+
   };
 
   return (
